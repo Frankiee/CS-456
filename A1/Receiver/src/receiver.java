0963631f3b355f619a75f2daaab29c0c697d2bfb
Frankiee <frank.ii@hotmail.com>
2012-02-26 15:52:01 -0500

    Version 1.00

---
+++ A1/Receiver/src/receiver.java
@@ -0,0 +1,150 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+/**
+ * CS 456 Assignment 1
+ *
+ * @author Frank Liu
+ * Student ID 20327548
+ *
+ * Date 2012.2.25
+ */
+
+public class receiver {
+    private InetAddress emuAdd;             // hostname for the network emulator
+    private int emuPort;                    // UDP port number used by the link emulator to receive ACKs from the receiver
+    private FileOutputStream fileStream;    // name of the file into which the received data is written
+
+    private final int dataPacketLength = 512;
+
+    private int expectedSeqNum = 0;
+    private DatagramSocket monitoringSocket;
+    private packet lastSentInOrderACKPacket;
+
+    private receiver (InetAddress emulatorAdd, int emulatorPort, int receiverPort, File fileToWriteTo) throws FileNotFoundException, SocketException {
+        emuAdd = emulatorAdd;
+        emuPort = emulatorPort;
+
+        fileStream = new FileOutputStream(fileToWriteTo);
+        monitoringSocket = new DatagramSocket(receiverPort);
+    }
+
+    public void startFileReceiving() throws RuntimeException, IOException, Exception {
+        byte[] receiveData = new byte[dataPacketLength];
+
+        while (true) {
+            // receive packet from sender
+            DatagramPacket receiveDatagramPacket = new DatagramPacket(receiveData, receiveData.length);
+            monitoringSocket.receive(receiveDatagramPacket);
+
+            packet rcvPacket = packet.parseUDPdata(receiveData);
+
+            checkReceivedPacketValidity(rcvPacket);
+
+            // received unexpected packet
+            if (!isReceivingExpectedPacket(rcvPacket)) {
+                // default: (re)send last-sent-in-order ACK packet
+                if (expectedSeqNum != 0)
+                    sendPacket(lastSentInOrderACKPacket);
+            } else {
+                if (rcvPacket.getType() == 1) {
+                    // update last-sent-in-order ACK packet
+                    lastSentInOrderACKPacket = packet.createACK(expectedSeqNum++);
+
+                    // write received packet to file
+                    writePacketToFile (rcvPacket);
+                    
+                    // send last-sent-in-order ACK packet
+                    sendPacket(lastSentInOrderACKPacket);
+                } else if (rcvPacket.getType() == 2) {
+                    sendPacket(rcvPacket);
+                    break;
+                }
+            }
+        }
+
+        fileStream.close();
+        monitoringSocket.close();
+    }
+
+    private void checkReceivedPacketValidity(packet p) throws RuntimeException {
+        if (p.getType() == 1) {
+            // data packet
+            if (p.getLength() <= 0)
+                throw new RuntimeException("receiver: Received data packet's data length is 0");
+
+        } else if (p.getType() == 2) {
+            // EOT packet
+            if (p.getLength() > 0)
+                throw new RuntimeException("receiver: EOT packet corrupted");
+        } else {
+            throw new RuntimeException("receiver: Undefined packet received: type " + p.getType());
+        }
+    }
+
+    private void writePacketToFile (packet rcvPacket) throws IOException {
+        fileStream.write(rcvPacket.getData());
+    }
+
+    private void sendPacket(packet p) throws IOException {
+        // send packet as byte array field of java DatagramPacket
+        byte[] sendData = p.getUDPdata();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, emuAdd, emuPort);
+
+        monitoringSocket.send(sendPacket);
+    }
+
+    private boolean isReceivingExpectedPacket(packet p) {
+        return p.getSeqNum() == expectedSeqNum % 32;
+    }
+
+    public static void main(String [ ] args) {
+
+        try {
+            // Check if the input format is valid, otherwise print usage description
+            if (args.length <= 4) {
+               String str = "Usage:\n"
+                   + "\tjava receiver | <arguments>\n\n"
+                   + "<arguments>:\n"
+                   + "\t<hostname for the network emulator>\n"
+                   + "\t<UDP port number used by the link emulator to receive ACKs from the receiver>\n"
+                   + "\t<UDP port number used by the receiver to receive data from the emulator>\n"
+                   + "\t<name of the file into which the received data is written>";
+               throw new RuntimeException(str);
+            }
+
+            InetAddress emulatorAdd = InetAddress.getByName(args[0]);
+            int emulatorPort = Integer.parseInt(args[1]);
+            int receiverPort = Integer.parseInt(args[2]);
+            File fileToWriteTo = new File(args[3]);
+
+            if (!fileToWriteTo.canWrite()) {
+                String str = "receiver: Given file is not writable";
+                throw new RuntimeException(str);
+            }
+
+            receiver fileReceiver = new receiver(emulatorAdd, emulatorPort, receiverPort, fileToWriteTo);
+            fileReceiver.startFileReceiving();
+
+        } catch (UnknownHostException ex) {
+            System.out.println("receiver: Can't resolve the host address of the network emulator: " + ex.getMessage());
+        } catch (RuntimeException ex) {
+            System.out.println(ex.getMessage());
+        } catch (FileNotFoundException ex) {
+            System.out.println("receiver: Given file not found: " + ex.getMessage());
+        } catch (SocketException ex) {
+            System.out.println("receiver: Could not create DatagramSocket (on given port) " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("receiver: FileOutputStream: I/O error" + ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("receiver: packet.createPacket: " + ex.getMessage());
+        }
+    }
+}
---
+++ A1/Sender/src/sender.java
@@ -0,0 +1,274 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * CS 456 Assignment 1
+ *
+ * @author Frank Liu
+ * Student ID 20327548
+ *
+ * Date 2012.2.25
+ */
+
+public class sender implements Runnable {
+    private final Object mux = new Object();
+
+    private final int ACKorEOTPacketLength = 12;
+
+    private FileTransmitter fileTransporter;
+
+    private final int windowSize = 10;
+    private int nextSeqNum = 0;
+    private int base = 0;
+
+    private DatagramSocket monitoringSocket;
+    private Map<Integer, packet> unacknowledgedPacketsCache = new HashMap<Integer, packet>();
+    private UnacknowledgedPacketsRetransmitTimer retransmitTimer = new UnacknowledgedPacketsRetransmitTimer();
+
+    private Thread ACKMonitoringThread;
+
+    private sender (FileTransmitter transp, int mtPort) throws SocketException {
+        fileTransporter = transp;
+
+        monitoringSocket = new DatagramSocket(mtPort);
+
+        // start monitoring thread
+        ACKMonitoringThread = new Thread(this);
+    }
+
+    // sender starts transmitting file
+    public void start() throws InterruptedException, IOException, Exception {
+        ACKMonitoringThread.start();        // start monitoring the ACK packet from receiver
+        startFileTransmitting();            // start transmitting file
+
+        ACKMonitoringThread.join();         // wait until monitoring thread finishes
+    }
+
+    private void startFileTransmitting() throws InterruptedException, IOException, Exception {
+        while (!fileTransporter.getIsFinished()) {
+            synchronized (mux) {
+                if (nextSeqNum < base + windowSize) {
+                    // read next chuck of file and create packet wrapper
+                    packet pkt = fileTransporter.readNextPacketFromFile(nextSeqNum);
+                    // push it to unacknowledged packets cache
+                    unacknowledgedPacketsCache.put(new Integer(nextSeqNum), pkt);
+
+                    // send packet
+                    fileTransporter.sendPacket(pkt);
+
+                    // reset count down timer
+                    if (base == nextSeqNum)
+                        retransmitTimer.reschedule();
+
+                    nextSeqNum++;
+                } else {
+                    mux.wait();
+                }
+            }
+        }
+    }
+
+    // Start monitoring the ACK packet sent from receiver
+    public void run() {
+        byte[] receivedData = new byte[ACKorEOTPacketLength];
+
+        try {
+            while (true) {
+                DatagramPacket receivedDatagramPacket = new DatagramPacket(receivedData, receivedData.length);
+                monitoringSocket.receive(receivedDatagramPacket);
+
+                packet receivedPacket = packet.parseUDPdata(receivedData);
+
+                // received ACK packet error checking
+                if (receivedPacket.getLength() != 0)
+                    throw new RuntimeException("pack length is not 0");
+
+                // update base and remove received packets from unacknowledged packets cache
+                int receivedPacketSeqNum = getSeqNumFromPacketSeqNum(receivedPacket.getSeqNum());
+                for (int i = base; i <= receivedPacketSeqNum; i++)
+                    unacknowledgedPacketsCache.remove(new Integer(i));
+
+                synchronized (mux) {
+                    base = Math.max(base, receivedPacketSeqNum + 1);
+
+                    // Update scheduled retransmitting task
+                    if (base == nextSeqNum) {
+                        retransmitTimer.cancelTask();
+                    } else {
+                        retransmitTimer.reschedule();
+                    }
+
+                    // base is changed, sender can continue sending packets
+                    mux.notifyAll();
+                }
+
+                if (receivedPacket.getType() == 2) {
+                    if (shouldFinishMonitoring())
+                        break;
+                    
+                    throw new RuntimeException("EOT packet received while FileTransmitter is not finished");
+                    
+                } else if (receivedPacket.getType() != 1) {
+                    throw new RuntimeException("undefined packet received: type " + receivedPacket.getType());
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("sender: Received packet corrupted:" + ex.getMessage());
+        }
+
+        // close monitoring and transmitting socket
+        monitoringSocket.close();
+        fileTransporter.closeTransmitterSocket();
+    }
+
+    private int getSeqNumFromPacketSeqNum(int packetSeqNum) {
+        int num = base - base % 32 + packetSeqNum;
+        return (num >= base)? num : num + 32;
+    }
+
+    private boolean shouldFinishMonitoring() {
+        synchronized (mux) {
+            return fileTransporter.getIsFinished() && nextSeqNum == base;
+        }
+    }
+
+    private class UnacknowledgedPacketsRetransmitTimer extends TimerTask {
+        private final int countDownDelay = 1000;
+        private Timer timer = new Timer();
+
+        public UnacknowledgedPacketsRetransmitTimer() {
+            super();
+        }
+
+        public void reschedule() {
+            cancelTask();
+            timer.schedule(this, countDownDelay);
+        }
+
+        public void cancelTask() {
+            cancel();
+            timer.cancel();
+        }
+
+        public void run() {
+            // resend all unacknowledged packet
+            for(Map.Entry<Integer, packet> unacknowledgedPacket : unacknowledgedPacketsCache.entrySet()) {
+                try {
+                    fileTransporter.sendPacket(unacknowledgedPacket.getValue());
+                } catch (IOException ex) {
+                    System.out.println("sender: UnacknowledgedPacketsRetransmitTimer: packet I/O error" + ex.getMessage());
+                }
+            }
+        }
+    }
+
+    public static void main(String [ ] args) {
+
+        try {
+            // Check if the input format is valid, otherwise print usage description
+            if (args.length <= 4) {
+               String str = "Usage:\n"
+                       + "\tjava sender | <arguments>\n\n"
+                       + "<arguments>:\n"
+                       + "\t<host address of the network emulator>\n"
+                       + "\t<UDP port number used by the emulator to receive data from the sender>\n"
+                       + "\t<UDP port number used by the sender to receive ACKs from the emulator>\n"
+                       + "\t<name of the file to be transferred>";
+               throw new RuntimeException(str);
+            }
+
+            InetAddress emulatorAdd = InetAddress.getByName(args[0]);
+            int emulatorPort = Integer.parseInt(args[1]);
+            int senderPort = Integer.parseInt(args[2]);
+            File fileToBeTransferred = new File(args[3]);
+
+            if (!fileToBeTransferred.canRead()) {
+                String str = "sender: Given file is not readable";
+                throw new RuntimeException(str);
+            }
+
+            FileTransmitter fileTransporter = new FileTransmitter(emulatorAdd, emulatorPort, fileToBeTransferred);
+            sender fileSender = new sender(fileTransporter, senderPort);
+            fileSender.start();
+            
+        } catch (UnknownHostException ex) {
+            System.out.println("sender: Can't resolve the host address of the network emulator: " + ex.getMessage());
+        } catch (NumberFormatException ex) {
+            System.out.println("sender: Can't parse the given port number: " + ex.getMessage());
+        } catch (RuntimeException ex) {
+            System.out.println(ex.getMessage());
+        } catch (FileNotFoundException ex) {
+            System.out.println("sender: Given file not found: " + ex.getMessage());
+        } catch (InterruptedException ex) {
+            System.out.println("sender: Process interrupted unexpectedly: " + ex.getMessage());
+        } catch (SocketException ex) {
+            System.out.println("sender: Could not create DatagramSocket (on given port) " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("sender: I/O error" + ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("sender: packet.createPacket: " + ex.getMessage());
+        }
+    }
+}
+
+class FileTransmitter {
+    private InetAddress emuAdd;         // host address of the network emulator
+    private int emuPort;                // UDP port number used by the emulator to receive data from the sender
+    private FileInputStream fileStream; // file to be transferred
+
+    private DatagramSocket transmitterSocket;
+
+    private boolean isFinished = false;
+    private final int fileBufferSize = 500;
+    private byte [] fileBuff = new byte [fileBufferSize];
+
+    public FileTransmitter (InetAddress emulatorAdd, int emulatorPort, File fileToBeTransferred) throws FileNotFoundException, SocketException {
+        emuAdd = emulatorAdd;
+        emuPort = emulatorPort;
+
+        fileStream = new FileInputStream(fileToBeTransferred);
+        transmitterSocket = new DatagramSocket();
+    }
+
+    public void sendPacket(packet p) throws IOException {
+        // send packet as byte array field of java DatagramPacket
+        byte[] sendData = p.getUDPdata();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, emuAdd, emuPort);
+
+        transmitterSocket.send(sendPacket);
+    }
+
+    public void closeTransmitterSocket() {
+        transmitterSocket.close();
+    }
+
+    public packet readNextPacketFromFile (int nextSeqNum) throws IOException, Exception {
+        // read data to byte buffer
+
+        int ret = fileStream.read(fileBuff);
+
+        // Last read just finishes reading the file
+        if (ret == -1) {
+            isFinished = true;
+            fileStream.close();
+            return packet.createEOT(nextSeqNum);
+        }
+
+        return packet.createPacket(nextSeqNum, new String(fileBuff, 0, ret));
+    }
+
+    public boolean getIsFinished() {
+        return isFinished;
+    }
+}
